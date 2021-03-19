69f254b7b73a04166f57ea2d5b1bd31332f19760
sharibeast <sharibeast@gmail.com>
2021-03-19 17:46:30 +0300

    API response

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import Axios from 'axios';
+
 
 function App() {
+  const [comments, setComments] = useState([]);
+
+  useEffect(() => {
+    fetchComments();
+  }, [])
+
+
+  const fetchComments = async () => {
+    const response = await Axios('https://jsonplaceholder.typicode.com/comments')
+    setComments(response.data)
+  }
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {
+        comments && comments.map(
+          comment => {
+            return (
+              <div key={comment.id} style={{ alignItems: 'center', margin: '20px 60px' }}>
+                <h4>{comment.name}</h4>
+                <p>{comment.email}</p>
+              </div>
+            )
+          }
+        )
+      }
     </div>
   );
 }
